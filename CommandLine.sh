55ebcfdc51ec1bad4bc82785042874e8fa9513a7
Matteo Rampolla <matteo.rampolla96@gmail.com>
2022-10-27 22:50:55 +0200

    CommandLine.sh

---
+++ CommandLine.sh
@@ -0,0 +1,9 @@
+arr=$(awk -F'\t' 'BEGIN {OFS = FS} { if (length($8) >  100 ) print $2} ' instagram_posts.csv | head -9)
+
+for i in $arr
+do :
+    if [ $i == -1 ];
+    then echo User was not found!;
+    else grep $i instagram_profiles.csv | awk '{print $1, $2, $3, $4}';
+    fi
+done
