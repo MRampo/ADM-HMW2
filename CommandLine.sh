67ea960b5c3cbdabb5eba0fe15bac58a5760b1bf
Matteo Rampolla <matteo.rampolla96@gmail.com>
2022-10-27 22:54:57 +0200

    Update CommandLine.sh

--- CommandLine.sh
+++ CommandLine.sh
@@ -3,7 +3,7 @@ arr=$(awk -F'\t' 'BEGIN {OFS = FS} { if (length($8) >  100 ) print $2} ' instagr
 for i in $arr
 do :
     if [ $i == -1 ];
-    then echo User was not found!;
+    then echo User was not found;
     else grep $i instagram_profiles.csv | awk '{print $1, $2, $3, $4}';
     fi
 done
